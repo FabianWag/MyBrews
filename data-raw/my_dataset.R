74154168e736d99cd8861e0bcdfb06b8b5bc1227
FabianWag <fabianwagner.ds@gmail.com>
2023-02-06 12:43:26 +0100

    created local file for completed brews. included an update function for the local file
    api call on brewfather is limited to 50 brews. currently i am at around so download itself would be enough. after 50, the function is needed to include all batches
    
    old batches are only updated when new batches are included... for single batch updates on the local file, i have to write another function

--- data-raw/my_dataset.R
+++ data-raw/my_dataset.R
@@ -1,11 +1,9 @@
 ## code to prepare `my_dataset` dataset goes here
 library(httr)
 library(jsonlite)
+library(purrr)
 
 
-# res = GET(url = "http://api.open-notify.org/astros.json")
-# res
-# http_status(res)
 
 UserID <- "1zeKdqvoURRGQtrXZndZGwYdV0j2"
 APIKey <- "3sRziL7Gk1dvKJf0OhHgKMVp7ZNQtmYC2AkOa7Plvf4lvnG6G1hjCfTSLD0ac4Qd"
@@ -16,23 +14,83 @@ getcompleted <- GET("https://api.brewfather.app/v1/batches",
                                  limit = 50),
                     authenticate(UserID, APIKey))
 
+
+getfermenting <- GET("https://api.brewfather.app/v1/batches",
+                     query = list(complete = TRUE,
+                                  status = c("Fermenting"),
+                                  limit = 5),
+                     authenticate(UserID, APIKey))
+
+getconditioning <- GET("https://api.brewfather.app/v1/batches",
+                       query = list(complete = TRUE,
+                                    status = c("Conditioning"),
+                                    limit = 5),
+                       authenticate(UserID, APIKey))
+
 rm(UserID, APIKey)
-# getfermenting <- GET("https://api.brewfather.app/v1/batches",
-#                      query = list(complete = TRUE,
-#                                   status = c("Fermenting"),
-#                                   limit = 5),
-#                      authenticate(UserID, APIKey))
-
-# getconditioning <- GET("https://api.brewfather.app/v1/batches",
-#                        query = list(complete = TRUE,
-#                                     status = c("Conditioning"),
-#                                     limit = 5),
-#                        authenticate(UserID, APIKey))
-
-
 
 completed <- content(getcompleted)
-rm(getcompleted)
+fermenting <- content(getfermenting)
+conditioning <- content(getconditioning)
+rm(getcompleted,
+   getfermenting,
+   getconditioning)
+
+
+
+### create file for completed brews. compare elements and update file if more brews have been complete
+
+call_id <- function(x, list){
+  id <- list %>%
+    pluck(x, "_id")
+  return(id)
+}
+
+compare_list <- function(old, new){
+  saved_ids <- 1:length(old) %>%
+    map_vec(call_id, old)
+
+  new_ids <- 1:length(new) %>%
+    map_vec(call_id, new)
+
+  ids <- list(saved_ids = saved_ids,
+              new_ids = new_ids)
+  return(ids)
+
+}
+
+
+update_list <- function(old, new){
+
+  ids <- compare_list(old, new)
+
+  updated <- append(new, old[!(ids$saved_ids %in% ids$new_ids)])
+
+  saveRDS(object = updated, file = "data/rawlists.rds")
+}
+### load data file with completed batches, check for updates
+#saveRDS(object = completed, file = "data/rawlists.rds")
+completed_saved <- readRDS("data/rawlists.rds")
+
+check <- compare_list(completed_saved, completed)
+
+
+
+#### check if ids of the completed brews are the same, if not, update file by adding older ids to the current donwloaded files
+#### I decided to do this, in case i changed something in brewfather batches, the older information tends to be overwritten
+#### this is not triggered if there is no new completed brew added... this might be a todo for the future
+
+if(!all.equal(check[[1]], check[[2]])){
+  update_list(completed_saved, completed)
+  rm(completed_saved, completed, check)
+  completed <- readRDS("data/rawlists.rds")
+} else {
+  rm(completed_saved, check)
+}
+
+
+
+
 
 
 
