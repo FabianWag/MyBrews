47574c0b7a5bfcb0a3906a823a99fcb25f4b9d0e
FabianWag <fabianwagner.ds@gmail.com>
2023-02-10 11:40:32 +0100

    data preprocessing should be done. there are errors and missing documentation entries. I have to change that. I guess the error comes from being unable to read the data.

--- data-raw/my_dataset.R
+++ data-raw/my_dataset.R
@@ -65,13 +65,19 @@ update_list <- function(old, new){
 
   ids <- compare_list(old, new)
 
-  updated <- append(new, old[!(ids$saved_ids %in% ids$new_ids)])
+  completed_saved <- append(new, old[!(ids$saved_ids %in% ids$new_ids)])
 
-  saveRDS(object = updated, file = "data/rawlists.rds")
+  save(completed_saved, file = "data/rawlists.rda")
 }
 ### load data file with completed batches, check for updates
 #saveRDS(object = completed, file = "data/rawlists.rds")
-completed_saved <- readRDS("data/rawlists.rds")
+
+if(!file.exists("data/rawlists.rda")){
+  completed_saved <- completed
+  save(completed_saved, file = "data/rawlists.rda")
+}
+
+load("data/rawlists.rda")
 
 check <- compare_list(completed_saved, completed)
 
@@ -84,7 +90,9 @@ check <- compare_list(completed_saved, completed)
 if(!all.equal(check[[1]], check[[2]])){
   update_list(completed_saved, completed)
   rm(completed_saved, completed, check)
-  completed <- readRDS("data/rawlists.rds")
+  load("data/rawlists.rda")
+  completed <- completed_saved
+  rm(completed_saved)
 } else {
   rm(completed_saved, check)
 }
@@ -100,7 +108,10 @@ if(!all.equal(check[[1]], check[[2]])){
 ### one for hops and one for malts
 
 get_basics <- function(x){
-  basics <- list(name = completed %>%
+  basics <- list(id = completed %>%
+                   pluck(x, "_id"),
+
+                 name = completed %>%
                    pluck(x, "name"),
 
                  style_type = completed %>%
@@ -149,72 +160,107 @@ get_basics <- function(x){
                    pluck(x, "recipe", "batchSize"))
 }
 
+
+sg_to_p <- function(x){(-1 * 616.868) + (1111.14 * x) - (630.272 * x^2) + (135.997 * x^3)}
+efficiency <- function(plato, fermentables, batchsize){
+  efficiency <- ((plato*(batchsize*10))/(fermentables*1000))*100
+}
+
 basics_data <- map_dfr(1:length(completed), get_basics) %>%
   mutate(brewdate = lubridate::as_datetime(brewdate/1000),
          bottlingdate = lubridate::as_datetime(bottlingdate/1000),
-         estOGplato = (-1 * 616.868) + (1111.14 * estOG) - (630.272 * estOG^2) + (135.997 * estOG^3))
+         estOGplato = sg_to_p(estOG),
+         measuredOGplato = sg_to_p(measuredOG),
+         estFGplato = sg_to_p(estFG),
+         measuredFGplato = sg_to_p(measuredFG),
+         measuredBhefficiency_german = efficiency(measuredOGplato, fermentables_kg_fromrecipe, batchsize_liter),
+         estBhefficiency_german = efficiency(estOGplato, fermentables_kg_fromrecipe, batchsize_liter))
 
 get_hops <- function(x){
   completed %>%
-    pluck(x, "batchHops")
+    pluck(x, "recipe", "hops")
 }
 
-get_hops(1)
-map_dfr(1, get_hops)
+create_hops_dat <- function(x){
+  id <- completed %>%
+    pluck(x, "_id")
+  dat <- map_dfr(x, get_hops) %>%
+    select(use, time, name, usage, origin, ibu, alpha, amount) %>%
+    mutate(id = id)
+}
 
-get_malts <- function(x){
+
+hops_dt <- map_dfr(1:length(completed), create_hops_dat)
+
+# get_malts <- function(x){
+#   completed %>%
+#     pluck(x, "batchFermentables")
+# }
+
+#map_dfr(1, get_malts)
+
+
+get_malt <- function(x){
   completed %>%
-    pluck(x, "batchFermentables")
+    pluck(x, "recipe", "fermentables")
 }
 
-map_dfr(1, get_malts)
-
-
-get_steps <- function(x){
-  completed %>%
-    pluck(x, "batchFermentables")
+create_malt_dat <- function(x){
+  id <- completed %>%
+    pluck(x, "_id")
+  dat <- map_dfr(x, get_malt) %>%
+    select(name, grainCategory, type, percentage, amount, origin, supplier, color) %>%
+    mutate(id = id)
 }
 
-## old code
+malt_dt <- map_dfr(1:length(completed), create_malt_dat)
 
-data_size <- length(completed)
-
-
-completed_batches <- data.frame(style = character(data_size),
-                                equipment = character(data_size),
-                                brewhouse_efficiency = numeric(data_size),
-                                abv = numeric(data_size),
-                                batch_start = TRUE)
-
-
-
-for(i in 1:data_size){
-  list <- completed[[i]]
-
-  if(is.null(list$recipe$style$name)){
-    completed_batches$style[i] <- "none selected"
-  } else {
-    completed_batches$style[i] <- list$recipe$style$name
-  }
-
-  if(is.null(list$recipe$equipment$name)){
-    completed_batches$equipment[i] <- "none selected"
-  } else {
-    completed_batches$equipment[i] <- list$recipe$equipment$name
-  }
-
-  completed_batches$brewhouse_efficiency[i] <- list$measuredEfficiency
-  completed_batches$abv[i] <- list$measuredAbv
-
-  completed_batches$batch_start[i] <- list$brewDate/1000
-}
-
-rm(completed, list, data_size, i)
-
-completed_batches$batch_start <- lubridate::as_datetime(completed_batches$batch_start)
-
-my_dataset <- completed_batches
-rm(completed_batches)
+my_dataset <- list(basics_data,
+                   hops_dt,
+                   malt_dt)
 
 usethis::use_data(my_dataset, overwrite = TRUE)
+
+## old code
+#
+# data_size <- length(completed)
+#
+#
+# completed_batches <- data.frame(style = character(data_size),
+#                                 equipment = character(data_size),
+#                                 brewhouse_efficiency = numeric(data_size),
+#                                 abv = numeric(data_size),
+#                                 batch_start = TRUE)
+#
+#
+#
+# for(i in 1:data_size){
+#   list <- completed[[i]]
+#
+#   if(is.null(list$recipe$style$name)){
+#     completed_batches$style[i] <- "none selected"
+#   } else {
+#     completed_batches$style[i] <- list$recipe$style$name
+#   }
+#
+#   if(is.null(list$recipe$equipment$name)){
+#     completed_batches$equipment[i] <- "none selected"
+#   } else {
+#     completed_batches$equipment[i] <- list$recipe$equipment$name
+#   }
+#
+#   completed_batches$brewhouse_efficiency[i] <- list$measuredEfficiency
+#   completed_batches$abv[i] <- list$measuredAbv
+#
+#   completed_batches$batch_start[i] <- list$brewDate/1000
+# }
+#
+# rm(completed, list, data_size, i)
+#
+# completed_batches$batch_start <- lubridate::as_datetime(completed_batches$batch_start)
+#
+# my_dataset <- completed_batches
+# rm(completed_batches)
+
+
 
